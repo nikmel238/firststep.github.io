ed5bd1b5a6a263d6c3ba7f06064ce8972c384829
nikmel238 <man.nikita0@yandex.ru>
2023-03-12 13:27:02 +0300

    get MightyGimball

--- HTML/Mighty Gumball/js/mightygumball.js
+++ HTML/Mighty Gumball/js/mightygumball.js
@@ -1,51 +1,62 @@
 /* JS для MightyGimball.html */
-/*
- * get the content of a JSON file using Ajax 
- *
- */
 
 // свойство позволяющее активировать скрипты после полной загрузки страницы
-window.onload = init;
+window.onload = function () {
+	// задаем интервал обновления данных
+	setInterval(handleRefresh, 1000);
+};
 
-// создаем функцию init, которая содержит в себе функцию getSales
-function init() {
-	getSales();
-}
+let lastReportTime = 0;
 
-function getSales() {
+// функция вызова JSONP
+function handleRefresh() {
+	// присваиваем переменной ссылку откуда брать данные
+	let url =
+		`http://gumball.wickedlysmart.com/?callback=updateSales` +
+		//отправляем только свежие данные
+		`&lastreporttime=` +
+		lastReportTime +
+		// избавляемся от кэширования
+		`&random=` +
+		new Date().getTime();
+	// создаем новый элемент script в DOM
+	let newScriptElement = document.createElement(`script`);
+	// присваиваем элементы атрибуты
+	newScriptElement.setAttribute('src', url);
+	newScriptElement.setAttribute('id', `jsonp`);
 
-	// задаем переменную с указанием адреса программы на сервере
-	var url = "http://gumball.wickedlysmart.соm/gumball/gumball.html";
-	// создаем объект и присваиваем его переменной
-	var request = new XMLHttpRequest();
-	// используем метод open, для обращения к программе на сервере, используя HTTP запрос GET
-	request.open("GET", url);
-	// создаем обработчик, который при получении кода 200 (ответ сервера, что все ОК), запускает функцию updateSales
-	request.onload = function() {
-		if (request.status == 200) {
-			updateSales(request.responseText);
-		}
-	};
-	// с помощью метода send отправляем запрос на сервер, при этом ничего не передаем ему
-	request.send(null);
-}
-
-function updateSales(responseText) {
-	// извлекаем из HTML элемент с id = sales
-	var salesDiv = document.getElementById("sales");
-	// создаем переменную sales, и с помощью метода parse преобразует полученную строку в объект (массив)
-	var sales = JSON.parse(responseText);
-	// создаем цикл, и для каждого элемента массива ...
-	for (var i = 0; i < sales.length; i++) {
-		var sale = sales[i];
-		// создаем элемент div в HTML
-		var div = document.createElement("div");
-		// обращаемся к атрибуту class и присваиваем ему значение saleItem
-		div.setAttribute("class", "saleItem");
-		// образуем содержимое для элемента div
-		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
-		// определяем элемент div как дочерний
-		salesDiv.appendChild(div);
+	// перезаписываем JSONP
+	// извлекаем элемент с id = jsonp
+	let oldScriptElement = document.getElementById(`jsonp`);
+	// извлекаем элемент head
+	let head = document.getElementsByTagName(`head`)[0];
+	// если старый элемент еще отсутствует ...
+	if (oldScriptElement === null) {
+		// вставляем в head - script
+		head.appendChild(newScriptElement);
+	} else {
+		// заменяем старый script на новый
+		head.replaceChild(newScriptElement, oldScriptElement);
 	}
 }
 
+function updateSales(sales) {
+	// извлекаем из HTML элемент с id = sales
+	var salesDiv = document.getElementById('sales');
+	// создаем цикл, и для каждого элемента массива ...
+	for (var i = 0; i < sales.length; i += 1) {
+		var sale = sales[i];
+		// создаем элемент div в HTML
+		var div = document.createElement('div');
+		// обращаемся к атрибуту class и присваиваем ему значение saleItem
+		div.setAttribute('class', 'saleItem');
+		// образуем содержимое для элемента div
+		div.innerHTML = `В магазине "${sale.name}" продано ${sale.sales} жвачек.`;
+		// определяем элемент div как дочерний
+		salesDiv.appendChild(div);
+	}
+
+	if (sales.length > 0) {
+		lastReportTime = sales[sales.length - 1].time;
+	}
+}
